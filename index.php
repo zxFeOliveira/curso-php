bb713da78aa1a80e03b20a3a7e6c28b6f67118dc
Felipe Oliveira <fbmithrandir@outlook.com>
2022-09-11 14:31:21 -0300

    Estruturas HTML e CSS prontas

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Curso PHP</title>
+</head>
+
+<body>
+
+  <header class="cabecalho">
+    <h1>Curso PHP</h1>
+    <h2>Índice de Exercícios</h2>
+  </header>
+
+  <main class="principal">
+
+    <div class="conteudo">
+      <nav class="modulos">
+        <div class="modulo verde">
+          <h3>Módulo 01 - Básico</h3>
+
+          <ul>
+            <li><a href="exercicio.php?dir=teste&file=teste">Exercício A </a></li>
+          </ul>
+        </div>
+    </div>
+
+
+
+    </nav>
+
+    </div>
+
+  </main>
+
+  <footer class="rodape">
+    <?= date('Y') ?>
+  </footer>
+
+</body>
+
+</html>
